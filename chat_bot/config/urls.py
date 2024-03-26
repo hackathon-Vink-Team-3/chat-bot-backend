0e0ab0715ba08a24a79ba5e9df484503e88b99f6
Andey Dogadkin <jvgger@gmail.com>
2024-03-26 23:48:01 +0300

    Added bot urls to main urls.py.

--- chat_bot/config/urls.py
+++ chat_bot/config/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/", include("src.apps.api.urls"), name='api')
+    path("api/v1/", include("src.apps.api.urls"), name="api"),
+    path("tg-bot/", include("src.apps.tg_bot.urls"), name="tg_bot"),
 ]
