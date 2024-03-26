995bb2d71315a6b257324ffe4c4d7539eca9923d
Andey Dogadkin <jvgger@gmail.com>
2024-03-26 23:47:17 +0300

    Added bot webhook endpoint.

---
+++ chat_bot/src/apps/tg_bot/views.py
@@ -0,0 +1,17 @@
+import http
+from telebot import types
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from src.apps.tg_bot.loader import bot
+
+
+@api_view(["POST"])
+def web_hook_tg_bot(request) -> Response:
+    """Эндпоинт для вебхука."""
+    try:
+        update = types.Update.de_json(request.data)
+        bot.process_new_updates([update])
+    except (TypeError, KeyError):
+        return Response(status=http.HTTPStatus.BAD_REQUEST)
+    return Response(status=http.HTTPStatus.OK)
