1fc41763ed50e7e6eb948e377d38fc0f793075a0
Andey Dogadkin <jvgger@gmail.com>
2024-03-29 00:34:55 +0300

    Added logger to tg_bot view.

--- chat_bot/src/apps/tg_bot/views.py
+++ chat_bot/src/apps/tg_bot/views.py
@@ -1,10 +1,13 @@
 import http
+import logging
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from telebot import types
 
 from src.apps.tg_bot.loader import bot
+
+logger = logging.getLogger(__name__)
 
 
 @api_view(["POST"])
@@ -13,6 +16,8 @@ def web_hook_tg_bot(request) -> Response:
     try:
         update = types.Update.de_json(request.data)
         bot.process_new_updates([update])
-    except (TypeError, KeyError):
+    except (TypeError, KeyError) as e:
+        logger.error(f"Bad request: {e}")
         return Response(status=http.HTTPStatus.BAD_REQUEST)
+    logger.info("Telegram Bot new update. Status 200.")
     return Response(status=http.HTTPStatus.OK)
