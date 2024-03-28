dffae444ce9f4b6b9c24883ddcc57cd1ff87a123
Andey Dogadkin <jvgger@gmail.com>
2024-03-28 05:32:57 +0300

    Added answers using templates.

--- chat_bot/src/apps/tg_bot/handlers/base.py
+++ chat_bot/src/apps/tg_bot/handlers/base.py
@@ -3,7 +3,7 @@ import logging
 from telebot import types, TeleBot
 
 from src.apps.tg_bot.keyboards import BotKeyboards
-from src.apps.tg_bot.templates import StartHelpTemplates
+from src.apps.tg_bot.templates import BaseTemplates
 from src.base.utils import log_exceptions
 
 logger = logging.getLogger(__name__)
@@ -12,10 +12,10 @@ logger = logging.getLogger(__name__)
 @log_exceptions(logger)
 def command_start_handler(message: types.Message, bot: TeleBot):
     """Отправить приветствие."""
-    popular_questions = StartHelpTemplates.get_popular_questions()
+    popular_questions = BaseTemplates.get_popular_questions()
     bot.send_message(
         chat_id=message.from_user.id,
-        text=StartHelpTemplates.START_MESSAGE,
+        text=BaseTemplates.START_MESSAGE,
         reply_markup=BotKeyboards.popular_questions_reply_markup(
             popular_questions
         ),
@@ -28,18 +28,27 @@ def command_help_handler(message: types.Message, bot: TeleBot):
     """Отправить инструкцию по использованию."""
     bot.send_message(
         chat_id=message.from_user.id,
-        text=StartHelpTemplates.HELP_MESSAGE,
+        text=BaseTemplates.HELP_MESSAGE,
     )
     logger.debug(f"Telegram bot send help message to {message.from_user.id}")
 
 
 def not_text_handler(message: types.Message, bot: TeleBot):
     """Ответ на все сообщения тип которых не текст."""
-    bot.send_message(chat_id=message.from_user.id, text="Я вас не понял.")
+    bot.send_message(
+        chat_id=message.from_user.id,
+        text=BaseTemplates.NOT_TEXT_MESSAGE,
+    )
 
 
-def cancel_any_state(call: types.CallbackQuery, bot: TeleBot):
+def cancel_any_state(call: types.CallbackQuery | types.Message, bot: TeleBot):
     """Сброс любого состояния."""
-    print(call.inline_message_id)
     bot.delete_state(call.from_user.id, call.from_user.id)
-    bot.send_message(chat_id=call.from_user.id, text="Состояние сброшено.")
+    if isinstance(call, types.Message):
+        bot.send_message(chat_id=call.chat.id, text="Состояние сброшено.")
+    else:
+        bot.answer_callback_query(call.id, text="Отмена")
+        bot.delete_message(
+            chat_id=call.message.chat.id,
+            message_id=call.message.id,
+        )
