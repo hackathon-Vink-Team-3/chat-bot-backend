89fc88ea90050c95ad5e0f2c4f7eb726319bc667
Andey Dogadkin <jvgger@gmail.com>
2024-03-28 04:15:52 +0300

    FIx feedback_gateway handler.

--- chat_bot/src/apps/tg_bot/handlers/feedback.py
+++ chat_bot/src/apps/tg_bot/handlers/feedback.py
@@ -1,12 +1,24 @@
+import logging
+
 from telebot import types, TeleBot
 
 from src.apps.tg_bot.keyboards import BotKeyboards
+from src.apps.tg_bot.templates import FeedbackTemplates
+from src.base.utils import log_exceptions
+
+logger = logging.getLogger(__name__)
 
 
+@log_exceptions(logger)
 def feedback_gateway(message: types.Message, bot: TeleBot):
     """Обработчик входа в состояние оценки бота."""
     bot.send_message(
         chat_id=message.from_user.id,
-        text="Пожалуйста оцените работу сервиса от 1 до 10.",
-        reply_markup=BotKeyboards.feedback_inline_markup(),
+        text=FeedbackTemplates.GATEWAY_MESSAGE,
+        reply_markup=BotKeyboards.feedback_inline_markup(
+            FeedbackTemplates.ASSESSMENTS
+        ).add(BotKeyboards.cancel_button()),
+    )
+    logger.debug(
+        f"Telegram Bot send feedback gateway message to {message.from_user.id}"
     )
