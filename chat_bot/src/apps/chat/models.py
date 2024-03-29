b727644192a22e55b449f1f77f96387637f6ae4a
Andey Dogadkin <jvgger@gmail.com>
2024-03-30 02:49:49 +0300

    Added chat model.

--- chat_bot/src/apps/chat/models.py
+++ chat_bot/src/apps/chat/models.py
@@ -1,3 +1,43 @@
+import uuid
+
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Chat(models.Model):
+    """Модель чат."""
+
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+    )
+    is_open = models.BooleanField(
+        default=True,
+        verbose_name="Чат открыт.",
+    )
+    is_support_connected = models.BooleanField(
+        default=False,
+        verbose_name="Поддержка подключена.",
+    )
+    created_date = models.DateTimeField(
+        auto_now=True,
+        editable=False,
+        verbose_name="Дата создания.",
+    )
+    user_id = models.ForeignKey(
+        to=User,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="chats",
+        verbose_name="Пользователь.",
+    )
+
+
+class Message(models.Model):
+    """Модель сообщения."""
+
+    pass
