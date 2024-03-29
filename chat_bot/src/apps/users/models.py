27768014e2020a7aaeae5b6836366ac2a9b08eab
Andey Dogadkin <jvgger@gmail.com>
2024-03-30 00:55:57 +0300

    Delete role model.

--- chat_bot/src/apps/users/models.py
+++ chat_bot/src/apps/users/models.py
@@ -1,16 +1,9 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 
 
 class CustomUser(AbstractUser):
     """
     Модель пользователя.
     """
-    pass
 
-
-class Role(models.Model):
-    """
-    Модель ролей.
-    """
     pass
