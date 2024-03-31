170d1336af6db9b41ca2a9ad241d3dea198d1425
Andey Dogadkin <jvgger@gmail.com>
2024-03-31 17:37:46 +0300

    Fix.

--- chat_bot/config/urls.py
+++ chat_bot/config/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/", include("src.apps.api.urls"), name='api')
+    path("api/v1/", include("src.apps.api.urls"), name="api"),
 ]
