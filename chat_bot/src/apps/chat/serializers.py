256d22e67ac81bd16d6082d577d5c4e90798b662
Andey Dogadkin <jvgger@gmail.com>
2024-04-01 18:18:30 +0300

    Upgrade chat app serializers.

--- chat_bot/src/apps/chat/serializers.py
+++ chat_bot/src/apps/chat/serializers.py
@@ -3,8 +3,56 @@ from rest_framework import serializers
 from src.apps.chat.models import Chat, Dialog, Message
 
 
-class DialogSerializer(serializers.ModelSerializer):
-    """Сериализатор Диалогов."""
+class MessageSerializer(serializers.ModelSerializer):
+    """Сериализатор Сообщений."""
+
+    class Meta:
+        model = Message
+        fields = (
+            "id",
+            "text",
+            "sender_type",
+            "created_date",
+            "chat_id",
+            "dialog_id",
+            "user_id",
+        )
+        read_only_fields = (
+            "id",
+            "sender_type",
+            "created_date",
+            "chat_id",
+            "dialog_id",
+            "user_id",
+        )
+
+
+class DialogDetailSerializer(serializers.ModelSerializer):
+    """Сериализатор Диалога."""
+
+    messages = MessageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Dialog
+        fields = (
+            "id",
+            "is_open",
+            "messages",
+            "is_support_connected",
+            "assessment",
+            "created_date",
+            "chat_id",
+        )
+        read_only_fields = (
+            "id",
+            "is_support_connected",
+            "created_date",
+            "chat_id",
+        )
+
+
+class DialogListSerializer(serializers.ModelSerializer):
+    """Сериализатор списка Диалогов."""
 
     first_message_text = serializers.SerializerMethodField(read_only=True)
 
@@ -31,11 +79,34 @@ class DialogSerializer(serializers.ModelSerializer):
         first_message = Message.objects.filter(dialog_id=obj.id).first()
         return first_message.text if first_message else None
 
+    def create(self, validated_data):
+        chat_uuid = validated_data["chat_id"]
+        open_dialog = Dialog.objects.filter(
+            chat_id=chat_uuid,
+            is_open=True,
+        ).first()
+        if open_dialog:
+            open_dialog.is_open = False
+            open_dialog.save()
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        is_open = validated_data.get("is_open")
+        if is_open:
+            open_dialog = Dialog.objects.filter(
+                is_open=True, chat_id=instance.chat_id
+            ).first()
+            if open_dialog:
+                raise serializers.ValidationError(
+                    detail={"error": "Открытый диалог уже существует."}
+                )
+        return super().update(instance, validated_data)
+
 
 class ChatSerializer(serializers.ModelSerializer):
-    """Сериализатор Чатов."""
+    """Сериализатор Чата."""
 
-    dialogs = DialogSerializer(many=True, read_only=True)
+    dialogs = DialogListSerializer(many=True, read_only=True)
 
     class Meta:
         model = Chat
@@ -46,27 +117,3 @@ class ChatSerializer(serializers.ModelSerializer):
             "user_id",
         )
         read_only_fields = ("__all__",)
-
-
-class MessageSerializer(serializers.ModelSerializer):
-    """Сериализатор Сообщений."""
-
-    class Meta:
-        model = Message
-        fields = (
-            "id",
-            "text",
-            "sender_type",
-            "created_date",
-            "chat_id",
-            "dialog_id",
-            "user_id",
-        )
-        read_only_fields = (
-            "id",
-            "sender_type",
-            "created_date",
-            "chat_id",
-            "dialog_id",
-            "user_id",
-        )
