7366de55cbb382f06cd523c2a3e624a3db13588e
Andey Dogadkin <jvgger@gmail.com>
2024-04-02 04:30:54 +0300

    Add base path to ws routs.

--- chat_bot/src/apps/chat/routing.py
+++ chat_bot/src/apps/chat/routing.py
@@ -3,5 +3,9 @@ from django.urls import re_path
 from src.apps.chat import consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/chat/(?P<chat_uuid>\w+)/$", consumers.ChatConsumer.as_asgi()),
+    re_path(
+        r"ws/chat/(?P<chat_uuid>[0-9a-f\-]+)/dialog/((?P<dialog_id>\w+)/)$",
+        consumers.ChatConsumer.as_asgi(),
+    ),
+    re_path(r".*", consumers.ChatConsumer.as_asgi()),
 ]
