1193442b4596cab8809b2705a99037b8fd9c1154
Andey Dogadkin <jvgger@gmail.com>
2024-04-06 05:55:50 +0300

    Added admin panel for settings model.

---
+++ chat_bot/src/apps/core/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from django.contrib.auth.models import Group
+
+from src.apps.core.models import YaGptSettings
+
+
+@admin.register(YaGptSettings)
+class LMSAdmin(admin.ModelAdmin):
+    """Админ панель для настроек модели."""
+
+    list_display = (
+        "id",
+        "title",
+        "catalog_id",
+        "temperature",
+        "base_prompt_text",
+        "base_error_answer",
+        "now_active",
+        "context_ttl_seconds",
+        "max_tokens",
+    )
+    list_display_links = "id", "title"
+    list_editable = "temperature", "now_active"
+    search_fields = "title", "employee__last_name"
+    search_help_text = "Поиск по названию."
+
+
+admin.site.unregister(Group)
