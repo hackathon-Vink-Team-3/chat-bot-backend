76f99570a38df7c438df930ef65cb9d599985c3d
Andey Dogadkin <jvgger@gmail.com>
2024-04-09 00:00:37 +0300

    Added admin message panel, fix admin model setting panel.

--- chat_bot/src/apps/chat/admin.py
+++ chat_bot/src/apps/chat/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from src.apps.chat.models import Message
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    """Админ панель для сообщений."""
+
+    list_per_page = 20
+    list_max_show_all = 40
+
+    list_display = (
+        "id",
+        "text",
+        "sender_type",
+        "created_date",
+        "chat",
+        "dialog",
+        "user",
+    )
+    list_display_links = ("id",)
