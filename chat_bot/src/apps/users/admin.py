78daa620a384279d6c0125475353401225404c13
Andey Dogadkin <jvgger@gmail.com>
2024-04-08 15:37:57 +0300

    Added base user admin.

--- chat_bot/src/apps/users/admin.py
+++ chat_bot/src/apps/users/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from src.apps.users.models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (
+            "Учетные данные",
+            {
+                "fields": ("username", "password"),
+            },
+        ),
+        (
+            "Персональная информация",
+            {"fields": ("first_name", "last_name", "email", "phone_number")},
+        ),
+        (
+            "Права доступа",
+            {
+                "fields": ("is_active", "is_staff", "is_superuser"),
+            },
+        ),
+    )
+    list_display = (
+        "id",
+        "username",
+        "first_name",
+        "last_name",
+        "phone_number",
+    )
+    list_display_links = (
+        "id",
+        "username",
+        "first_name",
+        "last_name",
+    )
+    search_fields = ("first_name", "last_name")
