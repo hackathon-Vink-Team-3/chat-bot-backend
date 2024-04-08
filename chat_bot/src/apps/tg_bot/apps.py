642159c4293843bce519aa6e4eed8f5e7493a2ce
Andey Dogadkin <jvgger@gmail.com>
2024-04-09 00:01:04 +0300

    Delete ready method from tg bot app.

--- chat_bot/src/apps/tg_bot/apps.py
+++ chat_bot/src/apps/tg_bot/apps.py
@@ -4,10 +4,3 @@ from django.apps import AppConfig
 class TGBotConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "src.apps.tg_bot"
-
-    def ready(self):
-        import os
-        from src.apps.tg_bot.loader import on_startup
-
-        if os.environ.get("RUN_MAIN"):
-            on_startup()
